32fce29b194a8c9678dfd9aba33490ec63418b8c
NiranjanNCR <111573715+NiranjanNCR@users.noreply.github.com>
2024-12-21 21:25:57 +0530

    Update homeSecTwo.js

--- src/Components/homeSecTwo.js
+++ src/Components/homeSecTwo.js
@@ -6,44 +6,44 @@ const HomeSecTwo = () => {
   return (
     <>
      <section className='sectionTwo'>
-     <h2>Classes</h2>
+     <h2>EXPLORE</h2>
          <div className='sectionTwoContainer'>
 
              <div className='sectionTwoItem'>
                  <img src={Item} alt="" />
-                 <h3>class</h3>
-                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, atque?</p>
+                 <h3>Beginner</h3>
+                 <p>It's specially design for the primary school students</p>
              </div>
              
              <div className='sectionTwoItem'>
                  <img src={Item} alt="" />
-                 <h3>class</h3>
-                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, atque?</p>
+                 <h3> Intermediate </h3>
+                 <p>It's your time to explore more about your future scope</p>
              </div>
 
              <div className='sectionTwoItem'>
                  <img src={Item} alt="" />
-                 <h3>class</h3>
-                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, atque?</p>
+                 <h3>Advance</h3>
+                 <p>Know every right steps to achieve your dream with fullproof strategy</p>
              </div>
 
              <div className='sectionTwoItem'>
                  <img src={Item} alt="" />
-                 <h3>class</h3>
-                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, atque?</p>
+                 <h3>Technical</h3>
+                 <p>Technical career Roadmap for your bright future</p>
              </div>
 
              <div className='sectionTwoItem'>
                  <img src={Item} alt="" />
-                 <h3>class</h3>
-                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, atque?</p>
+                 <h3>Non-Technical</h3>
+                 <p>Non-Technical career Roadmap for your bright future</p>
              </div>
-
+{/* 
              <div className='sectionTwoItem'>
                  <img src={Item} alt="" />
                  <h3>class</h3>
                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, atque?</p>
-             </div>
+             </div> */}
 
          </div>
      </section>
