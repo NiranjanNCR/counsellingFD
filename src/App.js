20f289f51cf6ae5fa9e4784cfa47ef53b4e4f29d
NiranjanNCR <111573715+NiranjanNCR@users.noreply.github.com>
2025-01-04 14:29:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./Components/NavigationBar";
-import Programs from "./Components/pages/Programs";
-import Home from "./Components/pages/Home";
-import About from "./Components/pages/About";
-import Contact from "./Components/pages/Resources";
-import Resources from "./Components/pages/Contact"; // Ensure this exists.
+import Programs from "./Components/page/Programs";
+import Home from "./Components/page/Home";
+import About from "./Components/page/About";
+import Contact from "./Components/page/Resources";
+import Resources from "./Components/page/Contact"; // Ensure this exists.
 
 
 const App = () => {
