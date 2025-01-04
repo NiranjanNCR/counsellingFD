78098305455cd83f31b6ceb97561835965fa54ca
NiranjanNCR <111573715+NiranjanNCR@users.noreply.github.com>
2025-01-04 14:05:41 +0530

    Update NavigationBar.js

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,66 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../css/style.css";
-// import HeroImg from "../../public/images/hero-img.jpg"
-import HeroImg from "../images/hero-img.jpg";
-import logo from "../images/logo PKD.png"
+import logo from "../images/logo PKD.png";
 import { FaBars } from "react-icons/fa";
 
-const NavigationBar = () => {
 
+const NavigationBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [listOpen, setListOpen] = useState(false);
 
-  const toggleMenu = () =>{
+  const toggleMenu = () => {
     setMenuOpen(!menuOpen);
-  }
-  const toggleProgram = () =>{
+  };
+  const toggleProgram = () => {
     setListOpen(!listOpen);
-  }
+  };
 
   return (
     <>
-      <nav className='mainNav'>
-       <div className='navContainer'>
-           <div className='logo'><img src={logo} alt="logo.." /></div>
-           <div className={`nav-Items ${menuOpen ? 'open' : ''}`}>
-               <ul>
-               <li><a href="#">Home</a></li>
-               <li><a href="#">About</a></li>
-               <li><a onClick={toggleProgram} href="#">Programs</a>
-               <div className={`program-list ${listOpen ? 'open' : '' }`} >
-               <ul>
-                   <li>DISC</li>
-                   <li>Myers-briggs Type Indicator</li>
-                   <li>Personality Factor Questionnaire</li>
-                   <li>Eysenck Personality Inventory  </li>
-                   <li>Enneagram test  </li>
-                   <li>Caliper</li>
-                   <li>Minnesota Multiphasic Personality Inventory </li>
-               </ul>
-               </div>
-               </li>
-               <li><a href="#">Resources</a></li>
-               <li><a href="#">Contact</a></li>
-               <button className='booking-btn'>Book session</button>
-               </ul>
-           </div>
-       <div className='menubtn' onClick={toggleMenu}><FaBars size={40}/></div> 
-       </div>
+      <nav className="mainNav">
+        <div className="navContainer">
+          <div className="logo">
+            <img src={logo} alt="logo.." />
+          </div>
+          <div className={`nav-Items ${menuOpen ? "open" : ""}`}>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <a onClick={toggleProgram} href="#!">
+                  Programs
+                </a>
+                <div className={`program-list ${listOpen ? "open" : ""}`}>
+                  <ul>
+                    <li>
+                      <Link to="/programs/disc">DISC</Link>
+                    </li>
+                    <li>
+                      <Link to="/programs/myers-briggs">Myers-Briggs Type Indicator</Link>
+                    </li>
+                    <li>
+                      <Link to="/programs/personality-factor">
+                        Personality Factor Questionnaire
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/programs/eysenck">
+                        Eysenck Personality Inventory
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/programs/enneagram">Enneagram Test</Link>
+                    </li>
+                    <li>
+                      <Link to="/programs/caliper">Caliper</Link>
+                    </li>
+                    <li>
+                      <Link to="/programs/minnesota">
+                        Minnesota Multiphasic Personality Inventory
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <Link to="/resources">Resources</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <button className="booking-btn">Book session</button>
+            </ul>
+          </div>
+          <div className="menubtn" onClick={toggleMenu}>
+            <FaBars size={40} />
+          </div>
+        </div>
       </nav>
 
-
-<main className='hero-section'>
-
-<div className='hero-left'>
-    <h1>Take your <span>First</span> Step <span>wisely</span></h1>
-    <p>Empower your journey to success build essential skills for growth gain unshakable con</p>
-</div>
-<div className='hero-image'><img src={HeroImg} alt="" /></div>
-
-</main>
-
     </>
-  )
-}
+  );
+};
 
 export default NavigationBar;
