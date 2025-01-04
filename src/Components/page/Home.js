7ab00608fd799690bc08ad1d742ebd83cc66c620
NiranjanNCR <111573715+NiranjanNCR@users.noreply.github.com>
2025-01-04 14:26:24 +0530

    Update Home.js

--- src/Components/page/Home.js
+++ src/Components/page/Home.js
@@ -1,1 +1,26 @@
+import React from 'react'
+import Footer from '../footer'
+import ContactForm from '../ContactForm'
+import NavigationBar from '../NavigationBar'
+import HomeSecOne from '../homeSecOne'
+import HomeSecTwo from '../homeSecTwo'
+import Mentors from '../Mentors'
+// import Link from 'react-router-dom'
+import HeroSection from '../HeroSection'
 
+
+const Home = () => {
+  return (
+    <>
+      <NavigationBar/>
+      <HeroSection/>
+      <HomeSecOne/>
+      <HomeSecTwo/>
+      <Mentors/>
+      <ContactForm/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
