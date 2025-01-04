624a7189aef6af36c6bd41d13c52106888e2b7f7
NiranjanNCR <111573715+NiranjanNCR@users.noreply.github.com>
2025-01-04 14:32:57 +0530

    Add files via upload

---
+++ src/Components/Disc.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+
+const Disc = () => {
+  return (
+    <>
+    <NavigationBar />
+      Discomp
+    </>
+  )
+}
+
+export default Disc
